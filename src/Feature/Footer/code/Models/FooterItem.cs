ef19d73f9762db953f720ac54dc6a99adf873278
ammu24 <amritav800@gmail.com>
2020-11-04 19:21:30 +0530

    Banner tiles Update and Footer Component

---
+++ src/Feature/Footer/code/Models/FooterItem.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FordIndia.Feature.Footer.Models
+{
+    public class FooterItem
+    {
+        public Item FooterHeader;
+        public List<Item> FooterLinks;
+           
+    }
+}
